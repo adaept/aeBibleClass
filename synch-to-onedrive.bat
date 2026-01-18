3a02e00d4cfa2bd0182c23b09de27316a5e22b32
peterennis <peterennis@yahoo.com>
2026-01-17 17:45:30 -0700

    Update synch-to-onedrive.bat
    Backup to OneDrive Backups\adaept\aeBibleClass folder that does not get moved, improved exclude list

--- synch-to-onedrive.bat
+++ synch-to-onedrive.bat
@@ -2,11 +2,14 @@
 setlocal
 
 set "SOURCE=C:\adaept\aeBibleClass"
-set "DESTINATION=%UserProfile%\OneDrive\Documents"
+set "DESTINATION=%UserProfile%\OneDrive\Backups\adaept\aeBibleClass"
 
 if not exist "%DESTINATION%" (
     md "%DESTINATION%"
 )
+
+REM Hydrate OneDrive files to avoid rsync misreads
+attrib -P "%DESTINATION%\*" /S /D
 
 REM Convert Windows paths to WSL format
 for /f "usebackq delims=" %%A in (`wsl wslpath -u "%SOURCE%"`) do set "WSL_SRC=%%A"
@@ -14,6 +17,26 @@ for /f "usebackq delims=" %%B in (`wsl wslpath -u "%DESTINATION%"`) do set "WSL_
 
 echo Syncing from WSL source: %WSL_SRC%
 echo Syncing to WSL dest:   %WSL_DST%
+
+REM Add this to the command line for a dry run
+REM --dry-run
+
+wsl -- bash -lc ^
+"rsync -a --update ^
+  --checksum ^
+  --itemize-changes ^
+  --info=stats1 ^
+  --exclude='**/venv/**' ^
+  --exclude='**/.git/**' ^
+  --exclude='**/node_modules/**' ^
+  --exclude='/~$*' ^
+  --exclude='**/~$*' ^
+  --exclude='**/~*.tmp' ^
+  --exclude='**/*.wbk' ^
+  \"%WSL_SRC%/\" \"%WSL_DST%/\""
+
+echo Sync complete.
+pause
 
 REM Now call rsync with properly escaped exclude patterns using ^ CMD line-continuation character
 REM --itemize-changes to show the filenames and rsync changes code:
@@ -28,22 +51,6 @@ REM 7	.	Owner unchanged
 REM 8	.	Group unchanged
 REM 9	.	ACL unchanged
 REM 10	.	Extended attributes unchanged
-
-wsl -- bash -lc ^
-"rsync -a --update ^
-  --itemize-changes ^
-  --info=stats1 ^
-  --exclude='*/venv/**' ^
-  --exclude='*/.git/**' ^
-  \"%WSL_SRC%/\" ^
-  \"%WSL_DST%/\""
-
-REM wsl -- bash -lc "rsync -a --update --info=name1,stats1 --exclude=\"*/venv/**\" \"%WSL_SRC%/\" \"%WSL_DST%/\""
-
-echo Sync complete.
-pause
-
-
 
 REM xcopy "%SOURCE%\*" "%DESTINATION%\" /E /I /Y /D /C
 
